8a21d4f4d415e373638bd1308cb736215b3c4788
Prateek Goel <prateek.goel.cs.2019@mitmeerut.ac.in>
2022-02-20 22:56:28 +0530

    changes example

--- Min or Sum.py
+++ Min or Sum.py
@@ -12,3 +12,23 @@ for i in range(31):
     if chk[i]:
         ans+=(1<<i)
 print(ans)
+
+'''
+Examples : 
+
+Input
+4-No of test cases
+3 - size of list
+1 3 2 - element of the list
+5
+1 2 4 8 16
+2
+6 6
+3
+3 5 6
+Output
+3
+31
+6
+7
+'''
